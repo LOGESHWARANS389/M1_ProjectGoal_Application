ff54c626bd98504cb57f6356b100748d8df0fb0c
LOGESHWARANS389 <98637796+LOGESHWARANS389@users.noreply.github.com>
2022-02-11 09:16:30 +0530

    Add files via upload

---
+++ 3_Implementation/edit.c
@@ -0,0 +1,52 @@
+#include<billing_system.h>
+void edit()
+{
+	FILE *fp;
+	FILE *fptr;
+	FILE *fpk;
+	int h;
+	int found=0;
+	int ka=0;
+	int p;
+	fp=fopen("Records.txt","r");
+	fptr=fopen("temp1.txt","w");
+	fpk=fopen("temp2.txt","w");
+	printf("\t");
+	printf("Enter the Product Number to Delete: ");
+	scanf("%d",&h);
+	printf("\n");
+	p=n;
+	while(!(ka==p))
+	{
+		ka++;
+		fscanf(fp,"%d",&item.productnumber);
+		if(item.productnumber==h)
+		{
+			found=1;
+			fscanf(fp,"%s",item.productname);
+			fscanf(fp,"%d",&item.quantity);
+			fscanf(fp,"%d",&item.price);
+			n--;
+			printf("\tProduct has been Successfully Deleted\n");
+		}
+		else
+		{
+			fscanf(fp,"%s",item.productname);
+			fscanf(fp,"%d",&item.quantity);
+			fscanf(fp,"%d",&item.price);
+			fprintf(fptr,"%d\t%s\t%d\t%d\t",item.productnumber,item.productname,item.quantity,item.price);
+		}
+	}
+	fprintf(fpk,"%d",n);
+	fclose(fpk);
+	fclose(fptr);
+	fclose(fp);
+	if(found==0)
+	{
+		printf("\tProduct no Found\n\n");
+	}
+	remove("Quantity.txt");
+	rename("temp2.txt","Quantity.txt");
+	remove("Records.txt");
+	rename("temp1.txt","Records.txt");
+}
