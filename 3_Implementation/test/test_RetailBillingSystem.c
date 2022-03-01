fde8fe553ee0215313dc49223c3bde8917f5a34e
LOGESHWARANS389 <98637796+LOGESHWARANS389@users.noreply.github.com>
2022-03-01 13:25:33 +0530

    Update test_RetailBillingSystem.c

--- 3_Implementation/test/test_RetailBillingSystem.c
+++ 3_Implementation/test/test_RetailBillingSystem.c
@@ -1,28 +1,22 @@
-#include "unity.h"
-#include <calculator_operations.h>
+#include"Billing_System.h"
+#include"unity_internals.h"
+#include"unity.h"
 
-/* Modify these two lines according to the project */
-#include <calculator_operations.h>
-#define PROJECT_NAME    "Calculator"
+#include <Billing_System.h>
+#define PROJECT_NAME    "RetailBillingSystemr"
 
-static fin_opt c1 = {0, 0, 0};
 
-/* Prototypes for all the test functions */
-void test_add(void);
-void test_subtract(void);
-void test_multiply(void);
-void test_divide(void);
-void test_simple(void);
-void test_compound(void);
-void test_total(void);
-void test_fact(void);
-void test_log(void);
-void test_exp(void);
-void test_power(void);
 
-/* Required by the unity test framework */
+void create()
+void bill()
+void display()
+void edit()
+void invoice()
+void mdelete()
+void mdisplay()
+
 void setUp(){}
-/* Required by the unity test framework */
+
 void tearDown(){}
 
 /* Start of the application test */
@@ -32,135 +26,87 @@ int main()
   UNITY_BEGIN();
 
 /* Run Test functions */
-  RUN_TEST(test_add);
-  RUN_TEST(test_subtract);
-  RUN_TEST(test_multiply);
-  RUN_TEST(test_divide);
-  RUN_TEST(test_simple);
-  RUN_TEST(test_compound);
-  RUN_TEST(test_total);
-  RUN_TEST(test_fact);
-  RUN_TEST(test_log);
-  RUN_TEST(test_exp);
-  RUN_TEST(test_power);
+  RUN_TEST(create);
+  RUN_TEST(bill);
+  RUN_TEST(display);
+  RUN_TEST(edit);
+  RUN_TEST(invoice);
+  RUN_TEST(mdelete);
+  RUN_TEST(mdisplay);
+
 
   /* Close the Unity Test Framework */
   return UNITY_END();
 }
  
-void test_add(void) {
-  TEST_ASSERT_EQUAL(30, add(10, 20));
+void test_create()(void) {
+  TEST_ASSERT_EQUAL(pass,enter_product_code);
   
-  TEST_ASSERT_EQUAL(15000, add(7500, 7500));
-
-  TEST_ASSERT_EQUAL(4, add(7,-3));
-
-  TEST_ASSERT_EQUAL(-10, add(-7,-3));
-
-  TEST_ASSERT_EQUAL(-1, add(2147483647,2147483647));  
-}
-
-void test_subtract(void) {
-  TEST_ASSERT_EQUAL(100, subtract(1000, 900));
-
-  TEST_ASSERT_EQUAL(130, subtract(100, -30));
-
-  TEST_ASSERT_EQUAL(-7, subtract(-10, -3));
-
-  TEST_ASSERT_EQUAL(-3, subtract(0, 3)); 
-}
-
-void test_multiply(void) {
-  TEST_ASSERT_EQUAL(0, multiply(1, 0));
+  TEST_ASSERT_EQUAL(pass,enter_product_name);
   
-  TEST_ASSERT_EQUAL(10, multiply(2, 5));
-
-  TEST_ASSERT_EQUAL(-10, multiply(-2, 5));
-
-  TEST_ASSERT_EQUAL(10, multiply(-2, -5));
-
-  TEST_ASSERT_EQUAL(-1, multiply(10, 2147483647));
-
-  TEST_ASSERT_EQUAL(-1, multiply(2147483647, 2147483647));
-}
-
-void test_divide(void) {
-  TEST_ASSERT_EQUAL(10, divide(21, 2));
-
-  TEST_ASSERT_EQUAL(1, divide(2, 2));
-
-  TEST_ASSERT_EQUAL(-1, divide(-2, 2));
-
-  TEST_ASSERT_EQUAL(1, divide(-2, -2));
-
-  TEST_ASSERT_EQUAL(0, divide(1, 0));
-}
-
-void test_simple(void){
-
-  c1.principal_amount = 1000;
-  c1.rate_of_interest = 2;
-  c1.time_of_period = 5;
-  TEST_ASSERT_EQUAL(100,simple_interest(&c1));
-
-  c1.principal_amount = 1100;
-  c1.rate_of_interest = 2;
-  c1.time_of_period = 5.5;
-  TEST_ASSERT_EQUAL(121,simple_interest(&c1));
-
-}
-
-void test_compound(void){
-  c1.principal_amount = 1000;
-  c1.rate_of_interest = 2;
-  c1.time_of_period = 2;
-  TEST_ASSERT_EQUAL(1040.4,compound_interest(&c1));
+  TEST_ASSERT_EQUAL(pass,enter_quantity);
   
-  c1.principal_amount = 10000;
-  c1.rate_of_interest = 10.25;
-  c1.time_of_period = 5;
-  TEST_ASSERT_EQUAL(16288.9,compound_interest(&c1));
+  TEST_ASSERT_EQUAL(pass,enter_price);
 }
 
-void test_total(void){
-  TEST_ASSERT_EQUAL(50000,total_amount(35000,15000));
-
-  TEST_ASSERT_EQUAL(325691,total_amount(311440,14251));
+void test_bill()(void) {
+  TEST_ASSERT_EQUAL(pass,enter_product_number);
+  
+  TEST_ASSERT_EQUAL(pass,enter_the_quantity);
+  
+  TEST_ASSERT_EQUAL(fail,enter_quantity(out of stock));
+  
 }
 
-void test_fact(void){
-  TEST_ASSERT_EQUAL(120, fact(5));
-
-  TEST_ASSERT_EQUAL(1, fact(0));
-
-  TEST_ASSERT_EQUAL(1,fact(-5));
-
-  TEST_ASSERT_EQUAL(-1,fact(20));
-
+void test_display()(void) {
+  TEST_ASSERT_EQUAL(pass,product_number|product_name|quantity|price);
+  
+  TEST_ASSERT_EQUAL(fail(no items in cart));
+  
 }
 
-void test_log(void){
-  TEST_ASSERT_EQUAL(1,logarithm(10));
 
-  TEST_ASSERT_EQUAL(9.3319,logarithm(2147483647));
 
-  TEST_ASSERT_EQUAL(0.7403,logarithm(5.5));
-
-  TEST_ASSERT_EQUAL(-1,logarithm(-5));
+void test_edit()(void) {
+  TEST_ASSERT_EQUAL(pass,enter_the_product_number_to_delete);
+  
+  TEST_ASSERT_EQUAL(pass(producthas been successfully deleted));
+  
+  TEST_ASSERT_EQUAL(fail(product not found));
 }
 
-void test_exp(void){
-  TEST_ASSERT_EQUAL(20.0855,exponential(3));
 
-  TEST_ASSERT_EQUAL(-1,exponential(300000));
-
-  TEST_ASSERT_EQUAL(-1,exponential(-500000));
+void test_invoice()(void) {
+  TEST_ASSERT_EQUAL(pass,product_number|product_name|quantity|amount);
+  
+  TEST_ASSERT_EQUAL(235,invoice(100,135);
+  
+  TEST_ASSERT_EQUAL(pass,thank_you_for_shopping);
+                    
+  TEST_ASSERT_EQUAL(pass,visit_again);
 }
 
-void test_power(void){
-  TEST_ASSERT_EQUAL(1,power(3,0));
+                    
+void test_mdelete()(void) {
+  TEST_ASSERT_EQUAL(pass,enter_the_product_number_to_be_deleted);
+  
+  TEST_ASSERT_EQUAL(pass(producthas deleted successfully));
+}
+                    
+void test_mdisplay()(void) {
+    TEST_ASSERT_EQUAL(pass,product_number|product_name|quantity|price);
+}               
 
-  TEST_ASSERT_EQUAL(0.0625,power(2,-4));
 
-  TEST_ASSERT_EQUAL(42.44,power(2.3,4.5));
+int main(){
+    UNITY_BEGIN();
+
+    RUN_TEST(test_create);
+    RUN_TEST(test_bill);
+    RUN_TEST(test_display);
+    RUN_TEST(test_edit);
+    RUN_TEST(test_invoice);
+    RUN_TEST(test_mdelete);
+    RUN_TEST(test_mdisplay);
+    return UNITY_END();
 }
