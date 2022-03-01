1bc25a535cc8ea74b12d370d59690fc3bbfbadb5
LOGESHWARANS389 <98637796+LOGESHWARANS389@users.noreply.github.com>
2022-03-01 12:16:37 +0530

    Update billing_system.h

--- 3_Implementation/inc/billing_system.h
+++ 3_Implementation/inc/billing_system.h
@@ -1,8 +1,16 @@
 #ifndef BILLING_SYSTEM_H_
 #define BILLING_SYSTEM_H_
+
 #include<stdio.h>
 #include<string.h>
 #include<ctype.h>
 #include<conio.h>
+
+void bill()
+void display()
+void edit()
+void invoice()
+void mdelete()
+void mdisplay()
 
 #endif
